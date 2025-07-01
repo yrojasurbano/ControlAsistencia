206b37689169f6ec075c9e474a630cf3301eab6a
Yimmy Rojas Urbano <yrojas@inei.com.pe>
2025-07-01 12:18:49 -0500

    subiendo cambios

--- asistencia-backend/app.js
+++ asistencia-backend/app.js
@@ -1,19 +1,26 @@
-require('dotenv').config();
 const express = require('express');
-const app = express();
+const fs = require('fs');
+const https = require('https');
 const cors = require('cors');
+
+const app = express();
 app.use(cors());
+app.use(express.json());
 
-const bodyParser = require('body-parser');
-const marcacionRoutes = require('./routes/marcacion');
+// Tu endpoint de asistencia
+app.post('/api/marcar', (req, res) => {
+    const { dni } = req.body;
+    console.log(`🟢 DNI recibido: ${dni}`);
+    res.json({ mensaje: 'Asistencia registrada con éxito' });
+});
 
-app.use(bodyParser.json());
-app.use('/api', marcacionRoutes);
+// Leer el certificado y la llave privada
+const options = {
+    key: fs.readFileSync('192.168.36.38-key.pem'),
+    cert: fs.readFileSync('192.168.36.38.pem')
+};
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, '../asistencia-front')));
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+// Servidor HTTPS
+https.createServer(options, app).listen(3000, () => {
+    console.log('🔒 Backend HTTPS corriendo en https://192.168.36.38:3000');
 });
